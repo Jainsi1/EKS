f44d3cf2a67f03a2e91040a631ffb679e57f2878
Mayank <mayank@macbookair.com>
2023-04-13 18:06:39 +0530

    Added Upgrade command instead of Install

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ terraform apply -auto-approve
 aws eks update-kubeconfig --region `terraform output eks_cluster_arn | cut -d':' -f4` --name `terraform output eks_cluster_name | tr -d '"'`
 export CLUSTER_NAME=`terraform output eks_cluster_name | tr -d '"'` && export AUTOSCALER_ROLE=`terraform output cluster-autosclaer-role-arn| tr -d '"'` && envsubst < cluster-autoscaler.yml > cluster-autoscaler-with-envs.yml
 kubectl create namespace utilities || true
-helm install ingress-nginx ingress-nginx/ -n utilities || echo "ingress controller already installed"
-helm install metrics-server metrics-server/ -n utilities || echo "metrics server already installed"
+helm upgrade -i ingress-nginx ingress-nginx/ -n utilities || echo "ingress controller already installed"
+helm upgrade -i metrics-server metrics-server/ -n utilities || echo "metrics server already installed"
 kubectl apply -f cluster-autoscaler-with-envs.yml
 kubectl patch deployment cluster-autoscaler \
   -n kube-system \
